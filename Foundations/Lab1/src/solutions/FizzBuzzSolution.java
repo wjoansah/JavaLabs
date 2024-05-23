b820d597594ebd279b248d1d1d73006cef5078a5
wjoansah <wonderful.owusu-ansah@amalitech.com>
2024-05-23 13:55:05 +0000

    lab1: refactor fizzbuzz solution

--- Foundations/Lab1/src/solutions/FizzBuzzSolution.java
+++ Foundations/Lab1/src/solutions/FizzBuzzSolution.java
@@ -1,16 +1,21 @@
 package solutions;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class FizzBuzzSolution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int number = -1;
 
         System.out.print("Enter a number: ");
-        int number = scanner.nextInt();
-
-        // the benefits of DRY can be applied here
-        // but will have little impact
+        try {
+            number = scanner.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input. Only numbers are allowed.");
+            System.out.println("Exiting...");
+            System.exit(1);
+        }
 
         if (number % 3 == 0 && number % 5 == 0) {
             System.out.println("FizzBuzz");
