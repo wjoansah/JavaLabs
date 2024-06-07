ea729bf706b286c4b67b8f11de228b41be612b58
wjoansah <wonderful.owusu-ansah@amalitech.com>
2024-06-07 08:55:39 +0000

    lab5(part three): add user entity

---
+++ Foundations/Lab5/src/main/java/org/wjoansah/lab5/user/Role.java
@@ -0,0 +1,5 @@
+package org.wjoansah.lab5.user;
+
+public enum Role {
+    USER, ADMIN;
+}
---
+++ Foundations/Lab5/src/main/java/org/wjoansah/lab5/user/User.java
@@ -0,0 +1,26 @@
+package org.wjoansah.lab5.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue
+    private Integer Id;
+    private String name;
+    private String email;
+    private String password;
+    private Role role;
+}
