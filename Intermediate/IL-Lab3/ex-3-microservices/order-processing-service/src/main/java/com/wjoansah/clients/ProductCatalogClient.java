700f0a01b50a7824c243fd922c43039dea3d1978
wjoansah <wonderful.owusu-ansah@amalitech.com>
2024-09-14 14:46:29 +0000

    add k8s specific application properties

--- Intermediate/IL-Lab3/ex-3-microservices/order-processing-service/src/main/java/com/wjoansah/clients/ProductCatalogClient.java
+++ Intermediate/IL-Lab3/ex-3-microservices/order-processing-service/src/main/java/com/wjoansah/clients/ProductCatalogClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "product-catalog-service", url = "http://localhost:8082")
+@FeignClient(name = "product-catalog-service", url = "${product-catalog-service.url}")
 public interface ProductCatalogClient {
     @GetMapping("/products/{id}")
     Product getProductById(@PathVariable("id") Integer id);
--- Intermediate/IL-Lab3/ex-3-microservices/order-processing-service/src/main/java/com/wjoansah/clients/UserServiceClient.java
+++ Intermediate/IL-Lab3/ex-3-microservices/order-processing-service/src/main/java/com/wjoansah/clients/UserServiceClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "user-service", url = "http://localhost:8081")
+@FeignClient(name = "user-service", url = "${user-service.url}")
 public interface UserServiceClient {
     @GetMapping("/users/exists/{id}")
     boolean userExists(@PathVariable("id") int id);
